9fb0b6567105b9b969050b72660dca7652f0a998
jos3duardo <jos3duardolopes@gmail.com>
2019-07-21 05:22:54 -0400

    guardando um dado no banco de dados

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -4,13 +4,40 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Task;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class TaskController
+ * @package AppBundle\Controller
+ * @Route("/task/")
+ */
 class TaskController extends Controller
 {
+
+    /**
+     * @Route("new")
+     */
+    public function  newAction(){
+        //cria um novo objeto
+        $task = new Task();
+        //passa os parametros do novo objeti
+        $task->setName("My First Task");
+
+        //cria uma instancia do entity manager
+        $em = $this->getDoctrine()->getManager();
+        //adiciona um objeto na fila para ser gravado no banco
+        $em->persist($task);
+        //grava no banco este objeto
+        $em->flush();
+
+        return new Response("Task has been created");
+    }
+
     /**
      * @Route("/task/{name}")
      */
--- src/AppBundle/Entity/Task.php
+++ src/AppBundle/Entity/Task.php
@@ -22,4 +22,38 @@ class Task
      * @ORM\Column(type="string")
      */
     private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+
 }
