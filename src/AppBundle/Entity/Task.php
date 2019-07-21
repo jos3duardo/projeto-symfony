74346934d93d8392a7043472f3623d0cba126f70
jos3duardo <jos3duardolopes@gmail.com>
2019-07-21 05:08:28 -0400

    criando o banco de dados com o console, criando um entity e criando uma tabela no banco de acrodo com os dados da entity

---
+++ src/AppBundle/Entity/Task.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tasks")
+ */
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+}
